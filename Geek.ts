7992a85423da56a1b2d5fbd62b5426479fed7935
TanyaJuravleva <krilltt@mail.ru>
2022-10-07 00:18:08 +0300

    Вернула

--- Geek.ts
+++ Geek.ts
@@ -1,76 +1,94 @@
 type Presentation = {
-    name: string;
-    slidesList: Slide[];
-};
-type Editor = {
-    presentation: Presentation;
-    selectedSlidesId: string[];
-    selectedObjectsId: string[];
-    isPreview: boolean;
-};
-
-type Slide = {
-    id: string;
-    background: string;
-    elementsList: SlideElement[];
-};
-
-type Background = {
-    color: string;
-};
-
-type Dimensions = {
-    width: number;
-    height: number;
-};
-
-type Position = {
-    xy: number;
-    dimensions: Dimensions;
-};
-
-type Coordinates = {
-    x: number;
-    y: number;
-};
-
-type Size = {
-    width: number;
-    height: number;
-};
-
-type SlideElement = {
-    id: string;
-    startingPoint: Coordinates;
-    size: Size;
-    typeOfElements: TextElement | PictureElement | FigureElement;
-};
-
-type TextElement = {
-    text: string[];
-    size: number;
-    color: string;
-    font: string;
-};
-
-type PictureElement = {
-    src: string;
-};
-
-type FigureElement = {
-    figureOfType: FigureShape;
-    figureColor: string;
-    borderWidth: number;
-    borderColor: string;
-    xy: Coordinates;
-};
-
-enum FigureShape {
-    Circle,
-    Triangle,
-    Rectangle,
+    slides: Slide[],
 }
 
+type Slide = {
+    layout: TextField,
+    areas: ChousenArea,
+    background: '#000',
+    element: {textField, picture, figure},
+}
+type ChousenArea= {
+    id: '',
+    typeOfElement: {figure, textField, picture},
+}
+
+type TextField = {
+    typeOfText: {header, mainText}
+}
+
+type Figure = {
+    typeOfFigure: {circle, triangle, rectangle},
+    fill: '#fff',
+    stroke: '#000',
+}
+
+type Circle = {
+    radius: 10,
+    x: 10,
+    y: 20,
+
+}
+
+type Triangle = {
+    x1: 15,
+    y1: 15,
+    x2: 25,
+    y2: 25,
+    x3: 35,
+    y3: 35,
+}
+
+type Rectangle = {
+    x: 10,
+    y: 20,
+    width: 20,
+    height: 10
+
+}
+
+type Header = {
+    type: 'text',
+    size: 20,
+    font_weight: 'bold',
+    font: 'Ariard',
+    color: '#000',
+
+}
+
+type MainText = {
+    type: 'text',
+    size: 12,
+    font_weight: 'normal',
+    font: 'Ariard',
+    color: '#000',
+
+}
+
+type Picture = {
+    type: 'image',
+    url: 'cat.png'
+}
+
+function addSlide(presentation: Presentation, newSlide: Slide): Presentation
+{
+    return {
+        slides: [
+            ... presentation.slides,
+            newSlide
+        ]
+    }
+}
+
+function deleteSlide(presentation: Presentation, oldSlide: Slide): Presentation
+{
+    presentation.slides.splice(1,1)
+    return {
+        slides: [
+            ... presentation.slides
+        ]
+    }
+}
 function doubleSlide(presentation: Presentation, slide: Slide): Presentation
 {
     return {

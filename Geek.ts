fbaf7cd49e5d9958d15715030b2326f68d036b81
TanyaJuravleva <krilltt@mail.ru>
2022-10-07 00:10:50 +0300

    Сделала типы

--- Geek.ts
+++ Geek.ts
@@ -1,94 +1,76 @@
 type Presentation = {
-    slides: Slide[],
-}
+    name: string;
+    slidesList: Slide[];
+};
+type Editor = {
+    presentation: Presentation;
+    selectedSlidesId: string[];
+    selectedObjectsId: string[];
+    isPreview: boolean;
+};
 
 type Slide = {
-    layout: TextField,
-    areas: ChousenArea,
-    background: '#000',
-    element: {textField, picture, figure},
-}
-type ChousenArea= {
-    id: '',
-    typeOfElement: {figure, textField, picture},
-}
+    id: string;
+    background: string;
+    elementsList: SlideElement[];
+};
 
-type TextField = {
-    typeOfText: {header, mainText}
-}
+type Background = {
+    color: string;
+};
 
-type Figure = {
-    typeOfFigure: {circle, triangle, rectangle},
-    fill: '#fff',
-    stroke: '#000',
-}
+type Dimensions = {
+    width: number;
+    height: number;
+};
 
-type Circle = {
-    radius: 10,
-    x: 10,
-    y: 20,
+type Position = {
+    xy: number;
+    dimensions: Dimensions;
+};
 
-}
+type Coordinates = {
+    x: number;
+    y: number;
+};
 
-type Triangle = {
-    x1: 15,
-    y1: 15,
-    x2: 25,
-    y2: 25,
-    x3: 35,
-    y3: 35,
-}
+type Size = {
+    width: number;
+    height: number;
+};
 
-type Rectangle = {
-    x: 10,
-    y: 20,
-    width: 20,
-    height: 10
+type SlideElement = {
+    id: string;
+    startingPoint: Coordinates;
+    size: Size;
+    typeOfElements: TextElement | PictureElement | FigureElement;
+};
 
-}
+type TextElement = {
+    text: string[];
+    size: number;
+    color: string;
+    font: string;
+};
 
-type Header = {
-    type: 'text',
-    size: 20,
-    font_weight: 'bold',
-    font: 'Ariard',
-    color: '#000',
+type PictureElement = {
+    src: string;
+};
 
-}
+type FigureElement = {
+    figureOfType: FigureShape;
+    figureColor: string;
+    borderWidth: number;
+    borderColor: string;
+    xy: Coordinates;
+};
 
-type MainText = {
-    type: 'text',
-    size: 12,
-    font_weight: 'normal',
-    font: 'Ariard',
-    color: '#000',
-
-}
-
-type Picture = {
-    type: 'image',
-    url: 'cat.png'
+enum FigureShape {
+    Circle,
+    Triangle,
+    Rectangle,
 }
 
-function addSlide(presentation: Presentation, newSlide: Slide): Presentation
-{
-    return {
-        slides: [
-            ... presentation.slides,
-            newSlide
-        ]
-    }
-}
-
-function deleteSlide(presentation: Presentation, oldSlide: Slide): Presentation
-{
-    presentation.slides.splice(1,1)
-    return {
-        slides: [
-            ... presentation.slides
-        ]
-    }
-}
 function doubleSlide(presentation: Presentation, slide: Slide): Presentation
 {
     return {
